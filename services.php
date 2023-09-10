bc3a75bd5fb8abd80824329fa2d91d587ffcdc26
denisg027 <denis.gauthier@retz-o-lution-informatique.fr>
2023-09-10 09:47:19 +0200

    Création de la page Services.php

---
+++ services.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+include 'Assets/includes/head.html';
+include 'Assets/includes/navbar.php';
+
+?>
+
+
+<main id="main">
+    <h1 class="services">Bienvenue au Garage Vincent Parrot : <br> Expert en Réparation Automobile</h1>
+    <div class="container">
+        <p class="services">
+            Fort de ses 15 années d'expérience dans le domaine de la réparation automobile, le Garage Vincent Parrot est votre partenaire de confiance pour tous vos besoins en matière de véhicules.
+            Nous sommes fiers de mettre notre expertise au service de votre tranquillité d'esprit, en vous offrant une gamme complète de services de haute qualité.</p>
+
+        <h3>Nos Services Incluent :</h3>
+
+        <ul class="services">
+            <li>
+                1. Réparation Mécanique et Entretien : Notre équipe de mécaniciens qualifiés est prête à résoudre tous les problèmes mécaniques de votre véhicule, qu'il s'agisse d'une réparation complexe ou d'un simple entretien. Nous utilisons des équipements de pointe pour garantir que votre voiture est entre de bonnes mains.
+            </li>
+            <li>
+                2. Décalaminage à l'Hydrogène : Nous utilisons une technologie de pointe pour éliminer les dépôts de carbone dans votre moteur grâce au décalaminage à l'hydrogène. Cela améliore les performances de votre moteur, réduit la consommation de carburant et prolonge la durée de vie de votre véhicule.
+            </li>
+            <li>
+                3. Diagnostic de Panne Automobile : Notre équipe est équipée des outils de diagnostic les plus récents pour identifier rapidement et efficacement les problèmes potentiels de votre véhicule. Nous sommes déterminés à vous faire économiser du temps et de l'argent en détectant les problèmes dès le départ.
+            </li>
+            <li>
+                4. Vidange du Moteur et Révision Constructeur : L'entretien régulier de votre véhicule est essentiel pour assurer sa longévité. Nous proposons des vidanges de moteur et des révisions constructeur conformes aux recommandations du fabricant.
+            </li>
+            <li>
+                5. Entretien et Recharge de la Climatisation : Pour des trajets confortables tout au long de l'année, nous offrons des services d'entretien de la climatisation et de recharge. Profitez d'un habitacle frais en été et d'une désembuage efficace en hiver.
+            </li>
+            <li>
+                6. Réparation et Remplacement de Pare-Brise : Un pare-brise endommagé peut compromettre votre sécurité. Nous réparons ou remplaçons les pare-brise de manière professionnelle et en conformité avec les normes de sécurité les plus strictes.
+            </li>
+            <li>
+                7. Vente de Voitures Neuves et d'Occasion : Si vous recherchez un nouveau véhicule, nous proposons une sélection de voitures neuves et d'occasion de haute qualité. Notre équipe est là pour vous conseiller et vous guider dans votre choix.
+            </li>
+        </ul>
+
+
+        <h3 class="services">Chez le Garage Vincent Parrot, nous nous engageons à fournir un service fiable, efficace et transparent pour tous les propriétaires de véhicules. Votre satisfaction est notre priorité absolue. Faites confiance à notre expérience et à notre savoir-faire pour prendre soin de votre voiture. Contactez-nous dès aujourd'hui pour planifier un rendez-vous ou pour en savoir plus sur nos services exceptionnels. Nous sommes là pour vous aider à prendre la route en toute confiance.</h3>
+
+
+        <div class="diapositive">
+            <img class="image" src="/Assets//Images/Services.png" alt="services">
+            <img class="image" src="/Assets//Images/Services1.png" alt="services1">
+            <img class="image" src="/Assets//Images/Services2.png" alt="services2">
+            <img class="image" src="/Assets//Images/Services3.png" alt="services3">
+        </div>
+    </div>
+
+
+
+
+</main>
+
+<?php
+include './Assets/includes/footer.html';
+?>
